598098db018ae59259f87768fc2971123b617c24
Wada Takeru <takeru.wada.1112@gmail.com>
2020-09-19 12:32:01 +0900

    add Count up component

---
+++ src/components/Count.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="decrement" type="button">-</button>
+    <span>{{state.num}}</span>
+    <button @click="increment" type="button">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+export default defineComponent({
+  name: 'Count',
+  setup(){
+    const state = reactive({
+      num: 0
+    })
+    const increment = () => {
+      state.num += 1
+    }
+    const decrement = () => {
+      state.num -= 1
+    }
+    return {
+      state,
+      increment,
+      decrement
+    }
+  },
+});
+</script>
+
+<style></style>
